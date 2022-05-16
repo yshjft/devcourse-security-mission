7be6bb3d5027df35c2fda12c96b336f1b155c871
hong yuseok <jericoh@naver.com>
2022-05-16 11:21:19 +0900

    first commit

---
+++ m1/src/test/java/com/prgrms/devcourse/M1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prgrms.devcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
