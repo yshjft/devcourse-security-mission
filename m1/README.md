ad34de7f6b66b61d7c90f8ebe2450f013077ecaf
hong yuseok <jericoh@naver.com>
2022-05-16 19:37:23 +0900

    미션1 & 해설

--- m1/README.md
+++ m1/README.md
@@ -1,1 +1,12 @@
 ## mission 1
+
+- [X] configure(AuthenticationManagerBuilder auth) 메소드 override
+    -[X] passwordEncoder는 NoOpPasswordEncoder로 사용함 
+    -[X] 기본 로그인 계정을 AuthenticationManagerBuilder 클래스를 통해 추가
+- [ ] 로그아웃, Cookie 기반 자동 로그인 (Remember-Me) 기능 설정하기
+    - [X] HttpSecurity 클래스의 logout() API를 통해 로그아웃 기능을 설정
+        - 로그아웃 처리 path “/logout”
+        - 로그아웃 성공 후 리다이렉션 path “/”  
+    - [ ] HttpSecurity 클래스의 rememberMe() API를 통해 Cookie 기반 자동 로그인 기능을 설정
+        - 파라미터명 “remember-me”
+        - 자동 로그인 토큰 유효기간 5분 @
---
+++ m1/src/main/java/com/prgrms/devcourse/configures/WebSecurityConfigure.java
@@ -0,0 +1,54 @@
+package com.prgrms.devcourse.configures;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
+    @Autowired
+    UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("{noop}user123").roles("USER")
+                .and()
+                .withUser("admin").password("{noop}admin123").roles("ADMIN");
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/assets/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .antMatchers("/me").hasAnyRole("USER", "ADMIN")
+                    .anyRequest().permitAll()
+                    .and()
+                .formLogin()
+                    .defaultSuccessUrl("/")
+                    .permitAll()
+                    .and()
+                .logout()
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                    .logoutSuccessUrl("/")
+                    .invalidateHttpSession(true)
+                    .clearAuthentication(true)
+                    .and()
+        ;
+    }
+}
